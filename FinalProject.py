f93b3686557a1407d0800eee688dc827ef3b4906
stephanieyounes <stephanie.younes@yahoo.com>
2021-12-08 18:22:59 -0600

    final steph code 12/8

--- FinalProject.py
+++ FinalProject.py
@@ -11,12 +11,32 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 USAfont = {'family': 'serif',
-        'color':  'darkblue',
+        'color':  '#15488D',
         'weight': 'normal',
         'size': 16,
         }
 CHINAfont = {'family': 'serif',
+        'color':  '#DF3B10',
+        'weight': 'normal',
+        'size': 16,
+        }
+MEXfont = {'family': 'serif',
         'color':  'darkred',
+        'weight': 'normal',
+        'size': 16,
+        }
+BRAfont = {'family': 'serif',
+        'color':  'white',
+        'weight': 'normal',
+        'size': 16,
+        }
+ITAfont = {'family': 'serif',
+        'color':  'white',
+        'weight': 'normal',
+        'size': 16,
+        }
+JORfont = {'family': 'serif',
+        'color':  'black',
         'weight': 'normal',
         'size': 16,
         }
@@ -30,12 +50,14 @@ usa_cases = df.loc[df["iso_code"] == 'USA',"total_cases"]
 chn_cases = df.loc[df["iso_code"] == 'CHN',"total_cases"]
 bra_cases = df.loc[df["iso_code"] == 'BRA',"total_cases"]
 ita_cases = df.loc[df["iso_code"] == 'ITA',"total_cases"]
+jor_cases = df.loc[df["iso_code"] == 'JOR',"total_cases"]
 
 mex_date = df.loc[df["iso_code"] == 'MEX', "date"]
 usa_date = df.loc[df["iso_code"] == 'USA', "date"]
 chn_date = df.loc[df["iso_code"] == 'CHN', "date"]
 bra_date = df.loc[df["iso_code"] == 'BRA', "date"]
 ita_date = df.loc[df["iso_code"] == 'ITA', "date"]
+jor_date = df.loc[df["iso_code"] == 'JOR', "date"]
 
 countries = ['Mexico','USA','China','Brazil','Italy']
 
@@ -70,6 +92,7 @@ COMPARING THE VACCINATION COUNT TO THE VIRUS' MORTALITY RATE OF THESE
 """
 
 #Read column names into array
+
 column_names = ["iso_code","continent","location","date","total_cases",
                 "new_cases","total_deaths","new_deaths","total_cases_per_million",
                 "new_cases_per_million","total_deaths_per_million",
@@ -80,7 +103,7 @@ column_names = ["iso_code","continent","location","date","total_cases",
                 ,"median_age","aged_65_older","aged_70_older","cardiovasc_death_rate"]
 
 
-#read file
+""" READ INFO FOR EACH COUNTRY"""
 df = pd.read_csv("Covid_Data_final.csv", names=column_names,low_memory=False,parse_dates=True)
 
 USA_new_deaths = df.loc[df["iso_code"] == 'USA',"new_deaths_per_million"]
@@ -89,77 +112,121 @@ USA_new_vaccinations = df.loc[df["iso_code"] == 'USA',"new_vaccinations_smoothed
 CHN_new_deaths = df.loc[df["iso_code"] == 'CHN',"new_deaths_per_million"]
 CHN_new_vaccinations = df.loc[df["iso_code"] == 'CHN',"new_vaccinations_smoothed"]
 
+MEX_new_deaths = df.loc[df["iso_code"] == 'MEX',"new_deaths_per_million"]
+MEX_new_vaccinations = df.loc[df["iso_code"] == 'MEX',"new_vaccinations_smoothed"]
+
+BRA_new_deaths = df.loc[df["iso_code"] == 'BRA',"new_deaths_per_million"]
+BRA_new_vaccinations = df.loc[df["iso_code"] == 'BRA',"new_vaccinations_smoothed"]
+
+ITA_new_deaths = df.loc[df["iso_code"] == 'ITA',"new_deaths_per_million"]
+ITA_new_vaccinations = df.loc[df["iso_code"] == 'ITA',"new_vaccinations_smoothed"]
+
+JOR_new_deaths = df.loc[df["iso_code"] == 'JOR',"new_deaths_per_million"]
+JOR_new_vaccinations = df.loc[df["iso_code"] == 'JOR',"new_vaccinations_smoothed"]
+
 
 """
-USA DEATHS PER DAY
+USA 
 """
-#plotting
-ax = plt.gca()
-ax.set_facecolor("tomato")
-plt.plot(usa_date,USA_new_deaths)
-# x-axis
+fig = plt.figure()
+fig.patch.set_facecolor('#F5755B')
+plt.xlabel("Date",fontdict=USAfont)
+plt.ylabel("Amount",fontdict=USAfont)
+plt.bar(usa_date,USA_new_deaths,label = 'new deaths',alpha=0.5,color="#1F5E97") 
+plt.bar(usa_date,USA_new_vaccinations,label = 'new vaccinations',alpha=0.5,color="#F66C28") 
+plt.bar(usa_date,usa_cases,label = 'new cases',alpha=0.5,color="#FAD4A2") 
+
+plt.legend(loc='upper right')
+plt.semilogy()
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
 plt.gca().xaxis.set_tick_params(rotation = 30)
-# y-axis
-plt.gca().yaxis.set_major_locator(mdates.DayLocator(interval=30)) 
-plt.ylabel("Deaths")
-plt.xlabel("Date")
-plt.title('USA Deaths Per Day', fontdict=USAfont)
+plt.title('USA Data Per Day', fontdict=USAfont)
 plt.show()
+"""
+CHINA 
+"""
+fig = plt.figure()
+fig.patch.set_facecolor('#E9E251')
+plt.xlabel("Date",fontdict=CHINAfont)
+plt.ylabel("Amount",fontdict=CHINAfont)
+plt.bar(chn_date,CHN_new_deaths,label = 'new deaths',alpha=0.5,color="#1F5E97") 
+plt.bar(chn_date,CHN_new_vaccinations,label = 'new vaccinations',alpha=0.5,color="#F66C28") 
+plt.bar(chn_date,chn_cases,label = 'new cases',alpha=0.5,color="#FAD4A2") 
 
-"""
-USA VACINNATIONS PER DAY
-"""
-ax = plt.gca()
-ax.set_facecolor("tomato")
-plt.plot(usa_date,USA_new_vaccinations)
-# x-axis
+plt.legend(loc='upper right')
+plt.semilogy()
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
 plt.gca().xaxis.set_tick_params(rotation = 30)
-# y-axis
-plt.gca().yaxis.set_major_locator(mdates.DayLocator(interval=30)) 
-#plotting
-
-plt.xlabel("Date")
-plt.ylabel("Vaccinations")
-plt.title('USA Vaccinations Per Day', fontdict=USAfont)
+plt.title("China Daya Per Day",fontdict=CHINAfont)
 plt.show()
 """
-CHINA DEATHS PER DAY
+MEXICO 
 """
-ax = plt.gca()
-ax.set_facecolor("sandybrown")
-plt.plot(chn_date,CHN_new_deaths)
-# x-axis
+fig = plt.figure()
+fig.patch.set_facecolor('#24770C')
+plt.xlabel("Date",fontdict=MEXfont)
+plt.ylabel("Amount",fontdict=MEXfont)
+plt.bar(mex_date,USA_new_deaths,label = 'new deaths',alpha=0.5,color="#1F5E97") 
+plt.bar(mex_date,USA_new_vaccinations,label = 'new vaccinations',alpha=0.5,color="#F66C28") 
+plt.bar(mex_date,usa_cases,label = 'new cases',alpha=0.5,color="#FAD4A2") 
+
+plt.legend(loc='upper right')
+plt.semilogy()
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
 plt.gca().xaxis.set_tick_params(rotation = 30)
-# y-axis
-plt.gca().yaxis.set_major_locator(mdates.DayLocator(interval=30)) 
-#plotting
-plt.xlabel("Date")
-plt.ylabel("Deaths")
-plt.title('China Deaths Per Day', fontdict=CHINAfont)
+plt.title('Mexico Data Per Day', fontdict=MEXfont)
 plt.show()
 """
-CHINA VACCINATIONS PER DAY
+BRAZIL 
 """
-ax = plt.gca()
-ax.set_facecolor("sandybrown")
-plt.plot(chn_date,CHN_new_vaccinations)
-# x-axis
+fig = plt.figure()
+fig.patch.set_facecolor('#0C6CA1')
+plt.xlabel("Date",fontdict=BRAfont)
+plt.ylabel("Amount",fontdict=BRAfont)
+plt.bar(bra_date,BRA_new_deaths,label = 'new deaths',alpha=0.5,color="#1F5E97") 
+plt.bar(bra_date,BRA_new_vaccinations,label = 'new vaccinations',alpha=0.5,color="#F66C28") 
+plt.bar(bra_date,bra_cases,label = 'new cases',alpha=0.5,color="#FAD4A2") 
+
+plt.legend(loc='upper right')
+plt.semilogy()
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
 plt.gca().xaxis.set_tick_params(rotation = 30)
-# y-axis
-#plt.gca().yaxis.set_major_locator(mdates.DayLocator(interval=30)) 
-
-#plotting
-plt.xlabel("Date")
-plt.ylabel("Vaccinations")
-plt.title('China Vaccinations Per Day', fontdict=CHINAfont)
+plt.title('Brazil Data Per Day', fontdict=BRAfont)
 plt.show()
+"""
+ITALY
+"""
+fig = plt.figure()
+fig.patch.set_facecolor('#E74C3C')
+plt.xlabel("Date",fontdict=ITAfont)
+plt.ylabel("Amount",fontdict=ITAfont)
+plt.bar(ita_date,ITA_new_deaths,label = 'new deaths',alpha=0.5,color="#1F5E97") 
+plt.bar(ita_date,ITA_new_vaccinations,label = 'new vaccinations',alpha=0.5,color="#F66C28") 
+plt.bar(ita_date,ita_cases,label = 'new cases',alpha=0.5,color="#FAD4A2") 
 
+plt.legend(loc='upper right')
+plt.semilogy()
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
+plt.gca().xaxis.set_tick_params(rotation = 30)
+plt.title('Italy Data Per Day', fontdict=ITAfont)
+plt.show()
+"""
+JORDAN
+"""
+fig = plt.figure()
+fig.patch.set_facecolor('#F22D1E')
+plt.xlabel("Date",fontdict=JORfont)
+plt.ylabel("Amount",fontdict=JORfont)
+plt.bar(jor_date,JOR_new_deaths,label = 'new deaths',alpha=0.5,color="#1F5E97") 
+plt.bar(jor_date,JOR_new_vaccinations,label = 'new vaccinations',alpha=0.5,color="#F66C28") 
+plt.bar(jor_date,jor_cases,label = 'new cases',alpha=0.5,color="#FAD4A2") 
 
-
+plt.legend(loc='upper right')
+plt.semilogy()
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=30)) 
+plt.gca().xaxis.set_tick_params(rotation = 30)
+plt.title('Jordan Data Per Day', fontdict=JORfont)
+plt.show()
 
 #%% Section 4 Natalie
 # Impact the vaccine has on the hospitalization rate of countries
